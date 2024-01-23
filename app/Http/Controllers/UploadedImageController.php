86aee96352498b7b478772c71b69878daa7b5bb9
TanjibQuad <tanjib@quadque.tech>
2024-01-23 11:33:54 +0600

    get image

--- app/Http/Controllers/UploadedImageController.php
+++ app/Http/Controllers/UploadedImageController.php
@@ -9,16 +9,23 @@ class UploadedImageController extends Controller
 {
     public function getImages()
     {
-        // $directory = Storage::url('public/uploads');
-        // $files = Storage::files($directory);
-        // dd($files);
         $fileUrl = Storage::disk('local')->files('public/uploads');
-        dd($fileUrl);
         $imageBaseUrl = [];
-        foreach ($fileUrl as $key => $path) {
-            array_push($imageBaseUrl, "https://emailmarketing.queleadscrm.com" . $fileUrl);
+        foreach ($fileUrl as $url) {
+            $path = "https://emailmarketing.queleadscrm.com/" . $url;
+            array_push($imageBaseUrl, $path);
         }
-        dd($imageBaseUrl);
-        return response()->json(['file_url' => $imageBaseUrl]);
+        if (count($imageBaseUrl) > 0) {
+            return response()->json([
+                'meassage' => 'success',
+                'status' => 200,
+                'file_url' => $imageBaseUrl
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'No data found',
+                'status' => 404
+            ], 404);
+        }
     }
 }

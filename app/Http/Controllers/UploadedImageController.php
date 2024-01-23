7e00b488ecd4d82bbf781d5c906cd8eba46e74ba
TanjibQuad <tanjib@quadque.tech>
2024-01-23 14:07:18 +0600

    delete image

--- app/Http/Controllers/UploadedImageController.php
+++ app/Http/Controllers/UploadedImageController.php
@@ -41,7 +41,7 @@ class UploadedImageController extends Controller
             $file_path = Storage::path($file_name);
             // dd($file_path);
             if ($file_path) {
-                $response = Storage::delete($file_path);
+                $response = unlink(public_path($file_path));
                 // dd($response);
                 if ($response) {
                     return response()->json([

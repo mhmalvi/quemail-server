2a42bc3e41a96bd0ba43211b4f6a43568ea9db7d
TanjibQuad <tanjib@quadque.tech>
2024-01-25 13:04:12 +0600

    send mail

--- app/Http/Controllers/SendMailController.php
+++ app/Http/Controllers/SendMailController.php
@@ -2,18 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\SendMailRequest;
 use Exception;
 use App\Mail\MarketingMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use App\Http\Requests\SendMailRequest;
+use Illuminate\Support\Facades\Storage;
 // use Illuminate\Support\Facades\Mail;
 
 class SendMailController extends Controller
 {
     public function send_mail(SendMailRequest $request)
     {
-        $request->validated();
+        // $request->validated();
         $email_content = [
             $subject = $request->subject,
             $template = $request->template,
@@ -25,41 +26,26 @@ class SendMailController extends Controller
         // dd($emails);
         // $emails = ['tanjib@quadque.tech', 'tanjibrubyat@gmail.com', 'zulker@quadque.tech'];
         // dd($emails);
-        foreach ($emails as $key => $email) {
-            // dd($value);
-            try {
-                
-                $result = Mail::to($email)->queue(new MarketingMail($email_content));
-                // dd(Mail::flushMacros());
-                if($result){
-                    // dd("wrong");
-                    return response()->json([
-                        'message'=>'Mail sent',
-                        'status'=>200,
-                        'data'=> $email
-                    ],200);
-                }else{
-                    return response()->json([
-                        'message' => 'Mail sent failed',
-                        'status' => 500,
-                        'data' => $email
-                    ], 500);
-                }
-            } catch (Exception $e) {
-                return response()->json([
-                    'message'=>'Email not valid',
-                    'status'=>500
-                ],500);
+        try {
+            foreach ($emails as $key => $email) {
+                // dd($value);
+                $result = Mail::to($email)->send(new MarketingMail($email_content));
             }
-            
-            
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Email not valid',
+                'status' => 500
+            ], 500);
         }
         // dispatch($job);
         echo "Mail send successfully !!";
     }
 
-    public function imageUrl(Request $request){
-        // dd($request->all());
-        
+    public function imageUrl(Request $request)
+    {
+        $path = $request->file('image')->store('uploads', 'public');
+        return response()->json([
+            'location' => "https://emailmarketing.queleadscrm.com" . Storage::url($path)
+        ]);
     }
 }

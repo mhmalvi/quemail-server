3ea36ba5f0630b6c2ee1e2a2734dd8c8726af71d
TanjibQuad <tanjib@quadque.tech>
2024-01-30 16:50:27 +0600

    email

--- app/Jobs/SendQueueEmail.php
+++ app/Jobs/SendQueueEmail.php
@@ -29,26 +29,8 @@ class SendQueueEmail implements ShouldQueue
      */
     public function handle(): void
     {
-        // dd("fdfdgfrgg");
-        // $data = User::all();
-        // dd($this->email_content[2]);
-        // $emails = $this->email_content[2];
-        // dd($emails);
-        // $emails = ['tanjib@quadque.tech', 'tanjibrubyat@gmail.com', 'zulker@quadque.tech'];
-        // dd($emails);
-        // $obj = new MarketingMail();
         foreach ($this->email_content[2] as $key => $value) {
             \Mail::to($value)->send(new MarketingMail($this->email_content, $this->file_urls ? $this->file_urls : ''));
         }
-
-        // foreach ($data as $key => $value) {
-        //     $input['email'] = $value->email;
-        //     $input['name'] = $value->name;
-        //     // \Mail::send('mail.Test_mail', [], function ($message) use ($input) {
-        //     //     $message->to($input['email'], $input['name'])
-        //     //         ->subject($input['subject']);
-        //     // });
-        //     Mail::to($this->details)->queue(new Mail());
-        // }
     }
 }

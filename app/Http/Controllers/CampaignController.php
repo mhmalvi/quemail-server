283b2247d0cefaf74922eaf7676cdcf670acd77f
Tanjib Rubyat <tanjibrubyat@quadque.tech>
2024-02-02 23:43:17 +0600

    mail-track

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Campaign;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use App\Http\Requests\StoreCampaignRequest;
 use App\Http\Requests\UpdateCampaignRequest;
 

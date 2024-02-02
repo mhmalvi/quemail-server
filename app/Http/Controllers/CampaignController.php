a42d653fafb5987f3aa8ed33e390a324257d5a39
Tanjib Rubyat <tanjibrubyat@quadque.tech>
2024-02-03 00:02:32 +0600

    mail-track

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -17,7 +17,7 @@ class CampaignController extends Controller
     {
         // logger(request()->all());
         \DB::table('campaigns')->where('email', $request->email)->update(['open' => 1]);
-        return response()->file(public_path("11.jpg"));
+        return response()->file(public_path("11.png"));
     }
     public function send_mail(Request $request)
     {

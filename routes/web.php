94b8986ad0fd5db8fda740e623ba4af546270c1e
Tanjib Rubyat <tanjibrubyat@quadque.tech>
2024-02-03 00:24:32 +0600

    mail-track

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('images',[CampaignController::class,'index'])->name('track_open');
+
 
 Route::get('/track-mail',function(){
 

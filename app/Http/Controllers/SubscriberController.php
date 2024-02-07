26a0f8b89f8a675f5769f45d726e469e7216180e
TanjibQuad <tanjib@quadque.tech>
2024-02-07 10:04:31 +0600

    unsubscribe

--- app/Http/Controllers/SubscriberController.php
+++ app/Http/Controllers/SubscriberController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UnsubscribeRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\EmailRecordsDetails;
 
 class SubscriberController extends Controller
 {
-    public function unsubscribe(Request $request)
+    public function unsubscribe(UnsubscribeRequest $request)
     {
         $result = DB::table('email_records_details')->where('recipients_mail', $request->email)->update(['subscribed_or_unsubscribed' => 0]);
         if ($result) {

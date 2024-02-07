f70f70cf12c9b3ea3d56dd38fb6a1d4d5bd26259
TanjibQuad <tanjib@quadque.tech>
2024-02-07 09:28:19 +0600

    unsubscribe

--- app/Http/Controllers/SubscriberController.php
+++ app/Http/Controllers/SubscriberController.php
@@ -8,20 +8,19 @@ use App\Models\EmailRecordsDetails;
 
 class SubscriberController extends Controller
 {
-    public function unsubscribe(Request $request, $email)
+    public function unsubscribe(Request $request)
     {
-        DB::table('email_records_details')->where('recipients_mail', $email)->update(['subscribed_or_unsubscribed' => 0]);
-        return response()->file(public_path('1x1.png'));
-        // if ($result) {
-        //     return response()->json([
-        //         'message' => 'Unsubscribed',
-        //         'status' => 201
-        //     ], 201);
-        // } else {
-        //     return response()->json([
-        //         'message' => 'Failed',
-        //         'status' => 500
-        //     ], 500);
-        // }
+        $result = DB::table('email_records_details')->where('recipients_mail', $request->email)->update(['subscribed_or_unsubscribed' => 1]);
+        if ($result) {
+            return response()->json([
+                'message' => 'Unsubscribed',
+                'status' => 201
+            ], 201);
+        } else {
+            return response()->json([
+                'message' => 'Failed',
+                'status' => 500
+            ], 500);
+        }
     }
 }

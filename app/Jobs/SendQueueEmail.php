c327edfbcac307c954823b25e2b41be78603bb42
TanjibQuad <tanjib@quadque.tech>
2024-02-08 11:57:25 +0600

    queleads unsubscribe

--- app/Jobs/SendQueueEmail.php
+++ app/Jobs/SendQueueEmail.php
@@ -34,6 +34,7 @@ class SendQueueEmail implements ShouldQueue
     public function handle()
     {
         // foreach ($this->email_content[2] as $key => $value) {
+            dd($this->id);
             \Mail::to($this->email)->send(new MarketingMail($this->email_content,$this->id, $this->email, $this->file_urls ? $this->file_urls : ''));
         // }
     }

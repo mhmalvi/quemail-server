9193fe7946f9255565ac87f6eeea27ac9ff8a88d
TanjibQuad <tanjib@quadque.tech>
2024-03-04 16:06:23 +0600

    mail schedule

---
+++ database/migrations/2024_03_04_094455_create_scheduled_mails_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('scheduled_mails', function (Blueprint $table) {
+            $table->id();
+            $table->string('email');
+            $table->integer('bounce_status')->comment("1=bounced, 0=not bounced");
+            $table->bigInteger('user_id');
+            $table->timestamp('schedule');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('scheduled_mails');
+    }
+};

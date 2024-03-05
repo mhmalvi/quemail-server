0d72a1e0c17fc137dc671dd244fde6c40bbc53bb
TanjibQuad <tanjib@quadque.tech>
2024-03-05 09:13:47 +0600

    scheduler

---
+++ app/Console/Commands/ScheduleBulkMail.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ScheduleBulkMail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:schedule-bulk-mail';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Bal Mail Schedule';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        
+    }
+}
--- app/Http/Requests/ScheduleMailRequest.php
+++ app/Http/Requests/ScheduleMailRequest.php
@@ -22,7 +22,7 @@ class ScheduleMailRequest extends FormRequest
     public function rules(): array
     {
         return [
-            "email.*"=> "required|regex:/(.+)@(.+)\.(.+)/i",
+            "email.*"=> "required",
             "schedule"=>"required",
             "user_id"=>"required",
             "template.*"=>"required",

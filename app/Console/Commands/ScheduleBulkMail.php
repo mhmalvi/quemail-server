14e5ce46a84b57c3bf366e041d5544e2ec7c992e
TanjibQuad <tanjib@quadque.tech>
2024-03-07 09:42:22 +0600

    email scheduling

--- app/Console/Commands/ScheduleBulkMail.php
+++ app/Console/Commands/ScheduleBulkMail.php
@@ -112,12 +112,12 @@ class ScheduleBulkMail extends Command
 
                 $db_date = Carbon::parse($email->schedule)->format('Y-m-d');
                 $today_date = Carbon::now()->format('Y-m-d');
-                print_r($db_date);
-                print_r($today_date);
+                // print_r($db_date);
+                // print_r($today_date);
                 $db_time = Carbon::parse($email->schedule)->format('H:i');
                 $today_time = Carbon::now()->format('H:i');
-                print_r($db_time);
-                print_r($today_time);
+                // print_r($db_time);
+                // print_r($today_time);
                 if ($db_date <= $today_date && $email->delivery_status == 0) {
                     if ($db_time <= $today_time) {
                         $mail = DynamicMail::where('user_id', $email->user_id)->first();

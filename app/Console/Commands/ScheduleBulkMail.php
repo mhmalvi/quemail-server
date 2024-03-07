87c5cd45f5668037b58f01ac15ac205b79dbf19a
TanjibQuad <tanjib@quadque.tech>
2024-03-07 09:23:18 +0600

    email scheduling

--- app/Console/Commands/ScheduleBulkMail.php
+++ app/Console/Commands/ScheduleBulkMail.php
@@ -146,7 +146,7 @@ class ScheduleBulkMail extends Command
                             ]);
                             if($email->bounce_status==0){
                                 Mail::to($email->email)->send(new ScheduledMarketingMail($email->subject,
-                                $email->template, $email->id, $email->email));
+                                $email->template, $email_records_details->id, $email->email));
                             }
                             
                         }

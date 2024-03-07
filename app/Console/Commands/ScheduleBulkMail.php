81d753910c7c26ce13796c827e0cf04d4a6b5317
TanjibQuad <tanjib@quadque.tech>
2024-03-07 10:43:25 +0600

    email scheduling

--- app/Console/Commands/ScheduleBulkMail.php
+++ app/Console/Commands/ScheduleBulkMail.php
@@ -71,6 +71,7 @@ class ScheduleBulkMail extends Command
                     $email_records_id = json_decode($email_records->id);
                     $email_records_count = json_decode($email_records->counts);
                 }
+                print_r($email_records_count);
                 if($email->bounce_status==0 && $email->delivery_status==0){
                         $email_records->counts = $email_records_count+1;
                     $email_records->save();

ea66812b1e3881fcbf0afd426d7a7de35090618d
Tanjib Rubyat <tanjibrubyat@quadque.tech>
2024-03-17 12:27:45 +0600

    test

--- resources/views/mail/scheduledMail.blade.php
+++ resources/views/mail/scheduledMail.blade.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     {{-- <img src="{{ route('track_open',[$id]) }}" /> --}}
-    <img src="{{ 'https://emailmarketing.queleadscrm.com' . '/track/user/' . $id }}" />
-    {{-- <table background="' . 'https://emailmarketing.queleadscrm.com' . '/track/user/' . $id . '"></table> --}}
+    {{-- <img src="{{ 'https://emailmarketing.queleadscrm.com' . '/track/user/' . $id }}" /> --}}
+    <table background={{ 'https://emailmarketing.queleadscrm.com' . '/track/user/' . $id }}></table>
     {!! $template !!}
 </body>
+
 </html>

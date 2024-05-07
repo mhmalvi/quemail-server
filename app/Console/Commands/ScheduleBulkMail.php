4c1492f892670e0db33ea92f7a47ec2dfe2f2312
TanjibQuad <tanjib@quadque.tech>
2024-05-07 08:44:25 +0600

    cron

--- app/Console/Commands/ScheduleBulkMail.php
+++ app/Console/Commands/ScheduleBulkMail.php
@@ -137,8 +137,8 @@ class ScheduleBulkMail extends Command
 
                 $db_date = Carbon::parse($email->schedule)->format('Y-m-d');
                 $today_date = Carbon::now()->format('Y-m-d');
-                print_r($db_date);
-                print_r($today_date);
+                // print_r($db_date);
+                // print_r($today_date);
                 $db_time = Carbon::parse($email->schedule)->format('H:i');
                 $today_time = Carbon::now()->format('H:i');
                 print_r($db_time);

320331db007273762e46299da672e03c5a0bf2f8
TanjibQuad <tanjib@quadque.tech>
2024-05-13 16:45:54 +0600

    schedule

--- app/Http/Controllers/MailScheduleController.php
+++ app/Http/Controllers/MailScheduleController.php
@@ -38,14 +38,10 @@ class MailScheduleController extends Controller
             $scheduleJob->number_of_mails = $number_of_mails;
             $scheduleJob->save();
             // dd($scheduleJob->id);
-            $chunked_email = array_chunk($request->email, 100,true);
-            $chunked_template = array_chunk($request->template, 100,true);
-            $chunked_subject = array_chunk($request->subject, 100,true);
-            for ($i = 0; $i < count($chunked_email); $i++) {
+            for ($i = 0; $i < count($request->email); $i++) {
                 $scheduler = new ScheduledMail();
-                if ($chunked_email[$i] != "undefined" || $chunked_subject[$i] != "undefined" || $chunked_email[$i] !=
-                "" || $chunked_subject[$i] != "") {
-                    $scheduler->email = $chunked_email[$i];
+                if ($request->email[$i] != "undefined" || $request->subject[$i] != "undefined" || $request->email[$i] != "" || $request->subject[$i] != "") {
+                    $scheduler->email = $request->email[$i];
                     // if (preg_match('/@.+\./', $request->email[$i])) {
                     //     $scheduler->bounce_status = 1; //// 1 = bounced
                     // } else {
@@ -53,8 +49,8 @@ class MailScheduleController extends Controller
                     // }
                     $scheduler->schedule = $request->schedule;
                     $scheduler->user_id = $request->user_id;
-                    $scheduler->template = $chunked_template[$i];
-                    $scheduler->subject = $chunked_subject[$i];
+                    $scheduler->template = $request->template[$i];
+                    $scheduler->subject = $request->subject[$i];
                     $scheduler->scheduled_jobs_id = $scheduleJob->id;
                     $scheduler->delivery_status = 0;
                     $scheduler->save();
